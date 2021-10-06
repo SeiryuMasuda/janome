9929f63da1655411112804f54131e1a078f7d9ac
SeiryuMasuda <91622047+SeiryuMasuda@users.noreply.github.com>
2021-10-06 15:23:32 +0900

    Update 字句解析.py

--- 字句解析.py
+++ 字句解析.py
@@ -7,4 +7,4 @@ text = input('文章を入力：')
 
 for token in t.tokenize(text):
     if token.part_of_speech.split(',')[0] == '名詞':
-    print(token.part_of_speech.split(',')[0], token.surface)
+        print(token.part_of_speech.split(',')[0], token.surface)
