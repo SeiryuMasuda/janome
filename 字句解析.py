bc6c4c07ae6b29da84d66fbfc69f83fe61f83584
SeiryuMasuda <91622047+SeiryuMasuda@users.noreply.github.com>
2021-10-06 15:08:34 +0900

    Add files via upload

--- 字句解析.py
+++ 字句解析.py
@@ -6,5 +6,5 @@ t = Tokenizer()
 text = input('文章を入力：')
 
 for token in t.tokenize(text):
-    #if token.part_of_speech.split(',')[0] == '名詞':
+    if token.part_of_speech.split(',')[0] == '名詞':
     print(token.part_of_speech.split(',')[0], token.surface)
